cc4e09daaef6f4e9d3e83fe68191b37b46bb883d
jair freitas <jairpg1993@gmail.com>
2023-02-18 10:39:03 -0300

    projeto iniciado

---
+++ src/test/java/br/com/jairfreitas/Restapi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jairfreitas.Restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
