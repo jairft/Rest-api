d6ca7157bbaf467455d01713b488f0a7b9e94e04
jair freitas <jairpg1993@gmail.com>
2023-02-18 11:17:18 -0300

    classe user resource findById

--- src/main/java/br/com/jairfreitas/Restapi/resources/UserResource.java
+++ src/main/java/br/com/jairfreitas/Restapi/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.jairfreitas.Restapi.resources;
 
 import br.com.jairfreitas.Restapi.domain.User;
+import br.com.jairfreitas.Restapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-
-        return ResponseEntity.ok().body(new User(1, "Jair", "jair@gmail.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
