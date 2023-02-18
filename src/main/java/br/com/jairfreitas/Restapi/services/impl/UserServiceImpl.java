8814a0f07f3921f2b9cd71b1d613d6079926b09f
jair freitas <jairpg1993@gmail.com>
2023-02-18 11:16:54 -0300

    classe user service impl findById

---
+++ src/main/java/br/com/jairfreitas/Restapi/services/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package br.com.jairfreitas.Restapi.services.impl;
+
+import br.com.jairfreitas.Restapi.domain.User;
+import br.com.jairfreitas.Restapi.repositories.UserRepository;
+import br.com.jairfreitas.Restapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
