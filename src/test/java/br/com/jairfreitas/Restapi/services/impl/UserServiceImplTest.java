f4362230c7e4e97bff3947addf8d5128262719c7
jair freitas <jairpg1993@gmail.com>
2023-02-22 14:13:56 -0300

    teste para classe user service, metodo update com sucesso

--- src/test/java/br/com/jairfreitas/Restapi/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/jairfreitas/Restapi/services/impl/UserServiceImplTest.java
@@ -119,7 +119,18 @@ class UserServiceImplTest {
     }
 
     @Test
-    void update() {
+    void quandoAtualizadoUmUsuarioRetorneSucesso() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.update(userDto);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+
     }
 
     @Test
